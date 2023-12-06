80361f7b83461e783cb46644cce6eaae405d9490
Your Name01~ <you@example.com>
2023-12-06 13:25:11 +0300

    refactor graph.js

--- claimarg_prototype/static/js/graph.js
+++ claimarg_prototype/static/js/graph.js
@@ -1,124 +1,134 @@
+function processEdgeColors(edges) {
+    return edges.map(edge => {
+        var colorMap = {
+            'proves': 'green',
+            'disproves': 'red',
+            'answers': 'purple',
+            'is_premise_of': 'orange'
+        };
+        var color = colorMap[edge.link_type] || 'black'; // Default color
+        return { ...edge, color: color };
+    });
+}
+
+function initializeNetwork(graphData, container) {
+    var options = {
+        edges: {
+            arrows: { to: { enabled: true, scaleFactor: 1, type: 'arrow' } }
+        }
+        // Other options as needed
+    };
+
+    var network = new vis.Network(container, graphData, options);
+    return network;
+}
+
+// Function to create a popup menu
+function createPopupMenu(nodeId, coordinates, container, graphData) {
+    var popup = createPopupElement(coordinates, container);
+
+    addDeleteButton(nodeId, popup, graphData);
+    addCloseButton(popup);
+
+    document.body.appendChild(popup);
+}
+
+function createPopupElement(coordinates, container) {
+    var popup = document.createElement("div");
+    popup.classList.add("popup-menu");
+
+    var position = calculatePopupPosition(coordinates, container);
+    popup.style.top = position.top + "px";
+    popup.style.left = position.left + "px";
+
+    return popup;
+}
+
+function calculatePopupPosition(coordinates, container) {
+    var containerRect = container.getBoundingClientRect();
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+    return {
+        top: coordinates.y + containerRect.top + scrollTop,
+        left: coordinates.x + containerRect.left + scrollLeft
+    };
+}
+
+function addDeleteButton(nodeId, popup, graphData) {
+    var deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = function() {
+        deleteNode(nodeId, graphData);
+        if (popup.parentNode) {
+            popup.parentNode.removeChild(popup);
+        }
+    };
+    popup.appendChild(deleteButton);
+}
+
+function addCloseButton(popup) {
+    var closeButton = document.createElement("button");
+    closeButton.innerHTML = "Close";
+    closeButton.onclick = function() {
+        if (popup.parentNode) {
+            popup.parentNode.removeChild(popup);
+        }
+    };
+    popup.appendChild(closeButton);
+}
+
+function deleteNode(nodeId, graphData) {
+    fetch('/core/delete_message/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify({ message_id: nodeId })
+    }).then(response => {
+        if (response.ok) {
+            graphData.nodes.remove(nodeId); // Remove the node from the graph
+        }
+    });
+}
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('/core/graph_data/')
         .then(response => response.json())
         .then(data => {
             var container = document.getElementById('network');
-
-            // Process edges to add colors
-            var edges = data.edges.map(edge => {
-                var color = '';
-                switch (edge.link_type) {
-                    case 'proves':
-                        color = 'green';
-                        break;
-                    case 'disproves':
-                        color = 'red';
-                        break;
-                    case 'answers':
-                        color = 'purple';
-                        break;
-                    case 'is_premise_of':
-                        color = 'orange';
-                        break;
-                    default:
-                        color = 'black'; // Default color
-                }
-                return { ...edge, color: color };
-            });
-
+            var processedEdges = processEdgeColors(data.edges);
             var graphData = {
                 nodes: new vis.DataSet(data.nodes),
-                edges: new vis.DataSet(edges)
+                edges: new vis.DataSet(processedEdges)
             };
 
-            var options = {
-                edges: {
-                    arrows: { to: { enabled: true, scaleFactor: 1, type: 'arrow' } }
-                }
-                // Other options as needed
-            };
+            var network = initializeNetwork(graphData, container);
 
-            var network = new vis.Network(container, graphData, options);
-
-            // Function to create a popup menu
-            function createPopupMenu(nodeId, coordinates) {
-                var popup = document.createElement("div");
-                popup.classList.add("popup-menu");
-                var containerRect = container.getBoundingClientRect();
-    
-                // Consider page scrolling
-                var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-                var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-
-                // Adjust coordinates with container's offset and scroll
-                var top = coordinates.y + containerRect.top + scrollTop;
-                var left = coordinates.x + containerRect.left + scrollLeft;
-
-                popup.style.top = top + "px";
-                popup.style.left = left + "px";
-
-                // Delete button
-                var deleteButton = document.createElement("button");
-                deleteButton.innerHTML = "Delete";
-                deleteButton.onclick = function() {
-                    deleteNode(nodeId);
-                    document.body.removeChild(popup); // Remove the popup after clicking
-                };
-                popup.appendChild(deleteButton);
-
-                // Close button
-                var closeButton = document.createElement("button");
-                closeButton.innerHTML = "Close";
-                closeButton.onclick = function() {
-                    document.body.removeChild(popup);
-                };
-                popup.appendChild(closeButton);
-
-
-                document.body.appendChild(popup);
-            }
-
-            // Function to delete a node
-            function deleteNode(nodeId) {
-                fetch('/core/delete_message/', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRFToken': getCookie('csrftoken') // Function to get CSRF token
-                    },
-                    body: JSON.stringify({ message_id: nodeId })
-                }).then(response => {
-                    if (response.ok) {
-                        graphData.nodes.remove(nodeId); // Remove the node from the graph
-                    }
-                });
-            }
-
-            // Function to get CSRF token
-            function getCookie(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie !== '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = cookies[i].trim();
-                        if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
-                    }
-                }
-                return cookieValue;
-            }
-
-            // Event listener for node clicks
             network.on("click", function(params) {
-                console.log("clicked");
                 if (params.nodes.length > 0) {
                     var nodeId = params.nodes[0];
                     var nodePosition = params.pointer.DOM;
-                    createPopupMenu(nodeId, nodePosition);
+                    createPopupMenu(nodeId, nodePosition, container, graphData);
                 }
             });
         });
 });
+
