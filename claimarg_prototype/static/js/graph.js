a42a92c1731da764553b8cf8c0db315429886612
Your Name01~ <you@example.com>
2023-12-08 23:52:58 +0300

    (*) [I-18] make graph width hierarchical

--- claimarg_prototype/static/js/graph.js
+++ claimarg_prototype/static/js/graph.js
@@ -7,15 +7,44 @@ function processEdgeColors(edges) {
             'is_premise_of': 'orange'
         };
         var color = colorMap[edge.link_type] || 'black'; // Default color
-        return { ...edge, color: color };
+        return { ...edge, 
+            color: color,
+            margin: 20, 
+        };
     });
 }
 
 function initializeNetwork(graphData, container) {
     var options = {
+        layout: {
+            hierarchical: {
+              enabled: true,
+              levelSeparation: 50,
+              edgeMinimization: true,
+              sortMethod: 'directed',
+              direction: 'DU',
+              blockShifting: false,
+              parentCentralization: true,
+              nodeSpacing: 300,
+              treeSpacing: 300,
+            },
+          },
+          physics: {
+            enabled: false,
+            hierarchicalRepulsion: {
+              nodeDistance: 0,
+              springLength: 0,
+              avoidOverlap: 0.5,
+            },
+          },
         edges: {
+            smooth: false,  // Make edges straight
+            physics: false,
             arrows: { to: { enabled: true, scaleFactor: 1, type: 'arrow' } }
         }
+        // physics: {
+        //     enabled: true  // Disable physics entirely
+        // }
         // Other options as needed
     };
 
