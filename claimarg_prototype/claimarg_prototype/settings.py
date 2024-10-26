a2f06f529034342d4dd91f840f49ff20dfba955c
Your Name <a@a>
2024-10-26 19:31:37 +0300

    configure STATIC_ROOT

--- claimarg_prototype/claimarg_prototype/settings.py
+++ claimarg_prototype/claimarg_prototype/settings.py
@@ -129,9 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = '/static/'
 
+# Point to the directory where your static files are located
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Directory where Django will collect all static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
